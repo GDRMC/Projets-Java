75662798dc0c642331594167f71d4752b12b47f2
GDRMC <gdrdu86@gmail.com>
2014-04-01 21:46:46 +0200

    QAxLoader.java / Projet QAxLoader

---
+++ QAxLoader-QAxLoader.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gdr.qaxloader;
+
+/**
+ *
+ * @author GDR
+ */
+public class QAxLoader {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Loader window = new Loader();
+        window.setVisible(true);
+    }
+}
