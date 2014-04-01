ad1d1cc6c9683beb7ee9818456d427cabe1a113f
GDRMC <gdrdu86@gmail.com>
2014-04-01 21:12:26 +0200

    QubazTs.java (main method) / Projet QubazTs

---
+++ QubazTs-QubazTs.java
@@ -0,0 +1,22 @@
+//Données Brutes Netbeans
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gdr.qubazts;
+
+/**
+ *
+ * @author GDR
+ */
+public class QubazTs {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainFrame window = new MainFrame();
+        window.setVisible(true);
+    }
+}
